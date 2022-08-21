7d2ed736c6dadf7f74107fe7fec7bd5d586bb919
Sam Wu <sjwu@ualberta.ca>
2022-08-20 21:48:16 -0600

    replace generic event text with custom event text

--- res/events.js
+++ res/events.js
@@ -1,30 +1,3 @@
-// using global window Array
-
-/*
-Event object
-    description: text for the event settings
-    options: maps strings indicating possible actions to outcomes
-*/
-window.eventPool = new Array(babyEvents, childEvents, teenEvents, adultEvents, seniorEvents);
-
-var babyEvents = new Array(babyEvent1);
-var babyEvent1 = {
-    description: "You spot a bottle containing some sort of white liquid. What do you do?",
-    options: new Map([
-        ["Drink it", babyOutcome1_1],
-        ["Taste it", babyOutcome1_2],
-        ["Point it out to your parents", babyOutcome1_3],
-    ])
-};
-
-var childEvents = new Array();
-
-var teenEvents = new Array();
-
-var adultEvents = new Array();
-
-var seniorEvents = new Array();
-
 /*
 Outcome object
     description: text for the results of the event
@@ -47,3 +20,54 @@ var babyOutcome1_3 = {
         You <span class='red'>improved your social skills</span> a bit. \
         Plus the milk makes your bones <span class='green'>stronger</span>."
 };
+
+/*
+Event object
+    description: text for the event settings
+    options: maps strings indicating possible actions to outcomes
+*/
+var babyEvent1 = {
+    description: "You spot a bottle containing some sort of white liquid. What do you do?",
+    options: new Map([
+        ["Drink it", babyOutcome1_1],
+        ["Taste it", babyOutcome1_2],
+        ["Point it out to your parents", babyOutcome1_3],
+    ])
+};
+var babyEvents = new Array(babyEvent1);
+
+var childEvents = new Array();
+
+var teenEvents = new Array();
+
+var adultEvents = new Array();
+
+var seniorEvents = new Array();
+
+window.eventPool = new Array(babyEvents, childEvents, teenEvents, adultEvents, seniorEvents);
+
+function waitForElement(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(() => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
+setup.replaceText = function(elementId, object) {
+    waitForElement('#'+elementId).then(() => {
+        document.getElementById(elementId).innerHTML = object.description;
+    });
+}
